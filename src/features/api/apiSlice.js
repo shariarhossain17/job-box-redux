472485a7afabe95eda9a86919f6760261f00d72d
shariarhossain23e <shariarhossain23@gamil.comm>
2022-12-26 17:52:55 +0600

    post user register with rtk query

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,10 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+    endpoints: (builder) => ({}),
+  });
 
-const appSlice = createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({}),
-    endpoints:(builder) => {
-        
-    }
-})
+export default apiSlice;
